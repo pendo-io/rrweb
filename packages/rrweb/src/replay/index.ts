2f598bf4b4d41bd8bff2210849ac325f1b996228
mikeyagner <mikeyagner@users.noreply.github.com>
2025-01-27 18:18:46 +0000

    Apply formatting changes

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -82,7 +82,7 @@ import {
   getPositionsAndIndex,
   uniqueTextMutations,
   StyleSheetMirror,
-  type ResolveTree
+  type ResolveTree,
 } from '../utils';
 import getInjectStyleRules from './styles/inject-style';
 import './styles/style.css';
@@ -1710,7 +1710,7 @@ export class Replayer {
         ids.add(tree.value.node.id);
         if (tree.children && tree.children.length > 0) {
           const res = nodeIdsToBeAdded(tree.children);
-          res.forEach(id => ids.add(id));
+          res.forEach((id) => ids.add(id));
         }
       }
       return ids;
